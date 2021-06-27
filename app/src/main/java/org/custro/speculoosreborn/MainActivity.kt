e60eea67b86a120551af45ccbc8a5602191c0404
SomeTiramisu <SomeTiramisu@users.noreply.github.com>
2021-06-27 18:45:00 +0200

    new fullsreen activity

--- app/src/main/java/org/custro/speculoosreborn/MainActivity.kt
+++ app/src/main/java/org/custro/speculoosreborn/MainActivity.kt
@@ -3,12 +3,17 @@ package org.custro.speculoosreborn
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+    }
+
+    fun startReader(view: View) {
         val intent = Intent(this, ReaderActivity::class.java)
         startActivity(intent)
     }
