71b3355aedd5d042dfa9068db1cc0189d470b50e
SomeTiramisu <SomeTiramisu@users.noreply.github.com>
2021-06-28 13:52:10 +0200

    libtiramisu reimplmented in Kotlin

---
+++ app/src/main/java/org/custro/speculoosreborn/libtiramisuk/Tiramisuk.kt
@@ -0,0 +1,46 @@
+package org.custro.speculoosreborn.libtiramisuk
+
+import org.custro.speculoosreborn.libtiramisuk.utils.PagePair
+import org.custro.speculoosreborn.libtiramisuk.utils.PageRequest
+import org.opencv.core.Mat
+
+class Tiramisuk {
+    private var mScheduler = PageScheduler()
+    private var mReq = PageRequest()
+    private var mSchedulerImageSlot: (Mat) -> Unit = {}
+    private var mBookSizeSlot: (Int) -> Unit = {}
+    private var mPreloaderProgressSlot: (Int) -> Unit = {}
+
+    init {
+        mScheduler.connectPageSlot {res: PagePair ->
+            if (mReq == res.req) {
+                mSchedulerImageSlot(res.img)
+            }
+        }
+        mScheduler.connectSizeSlot {res: Int ->
+            mBookSizeSlot(res)
+        }
+    }
+
+    fun get(req: PageRequest) {
+        if (req.file == null) {
+            mSchedulerImageSlot(Mat())
+            return
+        }
+        mReq = req
+        mScheduler.at(req)
+    }
+
+    fun connectPreloaderProgress(slot: (Int) -> Unit) {
+        mPreloaderProgressSlot = slot
+    }
+
+    fun connectBookSize(slot: (Int) -> Unit) {
+        mBookSizeSlot = slot
+    }
+
+    fun connectImage(slot: (Mat) -> Unit) {
+        mSchedulerImageSlot = slot
+    }
+
+}
