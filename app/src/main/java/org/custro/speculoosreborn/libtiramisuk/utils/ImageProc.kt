7d8f5c75ff4544c09973e0cf95afd83d2ce36725
SomeTiramisu <SomeTiramisu@users.noreply.github.com>
2021-06-28 16:12:08 +0200

    implement ImageProc

--- app/src/main/java/org/custro/speculoosreborn/libtiramisuk/utils/ImageProc.kt
+++ app/src/main/java/org/custro/speculoosreborn/libtiramisuk/utils/ImageProc.kt
@@ -1,11 +1,10 @@
 package org.custro.speculoosreborn.libtiramisuk.utils
 
-import org.opencv.core.CvType
-import org.opencv.core.Mat
-import org.opencv.core.MatOfByte
+import org.opencv.core.*
 import org.opencv.imgcodecs.Imgcodecs
 import org.opencv.imgproc.Imgproc
 import java.nio.ByteBuffer
+import kotlin.math.min
 
 fun fromByteArray(src: ByteArray): Mat {
     if (src.isEmpty()) {
@@ -17,4 +16,53 @@ fun fromByteArray(src: ByteArray): Mat {
     //val img = Imgcodecs.imread("/storage/emulated/0/000.jpg")
     Imgproc.cvtColor(img, img, Imgproc.COLOR_BGR2RGBA)
     return img
+}
+
+fun toPng(src: Mat): ByteArray {
+    TODO()
+}
+
+fun createMask(src: Mat, dst: Mat) {
+    Imgproc.cvtColor(src, dst, Imgproc.COLOR_RGBA2GRAY)
+    Imgproc.threshold(dst, dst, 240.0, 255.0, Imgproc.THRESH_BINARY_INV)
+}
+
+fun scale(src: Mat, dst: Mat, width: Int, height: Int) {
+    val imgWidth = src.cols()
+    val imgHeight = src.rows()
+    var fx = 1.0
+    var fy = 1.0
+
+    if (width==0 && height>0) {
+        fy = (height/imgHeight).toDouble()
+        fx = fy
+    }
+    if (height==0 && width>0) {
+        fx = (width/imgWidth).toDouble()
+        fy = fy
+    }
+    if (width>0 && height>0) {
+        fx = (width/imgWidth).toDouble()
+        fy = (height/imgHeight).toDouble()
+    }
+    val f = min(fx, fy)
+    if (f > 1) {
+        Imgproc.resize(src, dst, Size(), f, f, Imgproc.INTER_CUBIC)
+    } else {
+        Imgproc.resize(src, dst, Size(), f, f, Imgproc.INTER_AREA)
+    }
+}
+
+fun cropDetect(src: Mat): Rect {
+    val mask = Mat()
+    createMask(src, mask)
+    //TODO
+    return Rect()
+}
+fun cropScaleProcess(src: Mat, dst: Mat, roi: Rect, width: Int, height: Int) {
+    val tmp = Mat()
+    val mask = Mat()
+    var froi = if (roi.empty()) Rect(0, 0, src.cols(), src.rows()) else roi
+    scale(src.submat(froi), tmp, width, height)
+    tmp.copyTo(dst, mask)
 }
