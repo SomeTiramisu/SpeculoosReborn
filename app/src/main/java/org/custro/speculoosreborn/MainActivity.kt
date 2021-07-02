7963b5c4c9cc24ded6e660c7e825cfd19bd2ccfa
SomeTiramisu <SomeTiramisu@users.noreply.github.com>
2021-07-02 22:58:29 +0200

    request READ_EXTERNAL_STORAGE on startup

--- app/src/main/java/org/custro/speculoosreborn/MainActivity.kt
+++ app/src/main/java/org/custro/speculoosreborn/MainActivity.kt
@@ -1,13 +1,18 @@
 package org.custro.speculoosreborn
 
+import android.Manifest.permission.MANAGE_EXTERNAL_STORAGE
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment.getExternalStorageDirectory
 import android.util.Log
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.result.contract.ActivityResultContracts.OpenDocument
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.afollestad.materialdialogs.MaterialDialog
 import com.afollestad.materialdialogs.files.fileChooser
 import java.io.File
@@ -19,6 +24,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         System.loadLibrary("opencv_java4")
 
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (applicationContext.checkSelfPermission(READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED
+            || applicationContext.checkSelfPermission(MANAGE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+            requestPermissions(arrayOf(READ_EXTERNAL_STORAGE, MANAGE_EXTERNAL_STORAGE), 0)
+        }
         //startReader(null)
     }
 
