4e16677c7c4267d14a389723324ea7e45454e4d1
SomeTiramisu <SomeTiramisu@users.noreply.github.com>
2021-07-04 15:18:02 +0200

    WIP TapBox

--- app/src/main/java/org/custro/speculoosreborn/ReaderActivityCompose.kt
+++ app/src/main/java/org/custro/speculoosreborn/ReaderActivityCompose.kt
@@ -9,12 +9,15 @@ import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.input.pointer.pointerInput
 import org.custro.speculoosreborn.libtiramisuk.Tiramisuk
 import org.custro.speculoosreborn.libtiramisuk.utils.PageRequest
 import org.opencv.android.Utils
@@ -27,6 +30,7 @@ class ReaderActivityCompose : ComponentActivity() {
         hideSystemUi()
         setContent {
             ReaderView(file = File("/storage/emulated/0/aoe.cbz"))
+            TapBox()
         }
         //val (w, h) = getMetrics()
         //val req = PageRequest(0, w, h, File("/storage/emulated/0/aoe.cbz"))
@@ -44,7 +48,7 @@ class ReaderActivityCompose : ComponentActivity() {
     }
 
     @Composable
-    fun ReaderView(index: Int = 0, file: File) {
+    fun ReaderView(/*index: Int = 0, */file: File) {
         val (w, h) = remember { getMetrics() }
         val background = remember {
             val bitmap = BitmapFactory.decodeStream(assets.open("background.png"))
@@ -71,10 +75,27 @@ class ReaderActivityCompose : ComponentActivity() {
         )
         val firstImage = remember { //need to be at the end, i don't know why
             mTiramisu.connectImageCallback(imageCallback)
-            val req = PageRequest(index, w, h, file)
+            val req = PageRequest(index.value, w, h, file)
             mTiramisu.get(req)
             true
         }
+    }
+
+    @Composable
+    fun TapBox() {
+        val (w, h) = remember { getMetrics() }
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .pointerInput(Unit) {
+                detectTapGestures { offset ->  //onTap
+                    Log.d("TapBox", "Touched")
+                    if (offset.x > 2 * w / 3 /*&& index < bookSize - 1*/) {
+                        index.value += 1
+                    } else if (offset.x < w / 3 /*&& index > 0*/) {
+                        index.value -= 1
+                    }
+                }
+            })
     }
 
     private fun getMetrics(): Pair<Int, Int> {
@@ -87,6 +108,7 @@ class ReaderActivityCompose : ComponentActivity() {
         return Pair(width, height)
     }
 
+    private val index = mutableStateOf(0)
     companion object {
         private val mTiramisu = Tiramisuk()
     }
