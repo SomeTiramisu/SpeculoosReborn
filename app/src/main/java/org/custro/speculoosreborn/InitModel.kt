9ea7e34ebe4d7b41c390e8ec2b5416d28bc925cb
SomeTiramisu <SomeTiramisu@users.noreply.github.com>
2021-09-28 20:55:27 +0200

    Fix crash when reading corrupted manga entry

--- app/src/main/java/org/custro/speculoosreborn/InitModel.kt
+++ app/src/main/java/org/custro/speculoosreborn/InitModel.kt
@@ -1,24 +1,29 @@
 package org.custro.speculoosreborn
 
-import android.database.sqlite.SQLiteConstraintException
-import android.net.Uri
 import android.util.Log
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.room.Room
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import org.custro.speculoosreborn.libtiramisuk.utils.MangaParser
-import org.custro.speculoosreborn.libtiramisuk.utils.PageCache
-import org.custro.speculoosreborn.room.AppDatabase
 import org.custro.speculoosreborn.room.Manga
-import org.custro.speculoosreborn.room.genManga
+import org.custro.speculoosreborn.room.correctManga
+import org.custro.speculoosreborn.room.isMangaValid
 
 class InitModel: ViewModel() {
 
     fun getMangas(): LiveData<List<Manga>> {
-        return App.db.mangaDao().getAll()
+        return  Transformations.map(App.db.mangaDao().getAll()) { it ->
+            it.map { m ->
+                viewModelScope.launch(Dispatchers.Default) {
+                    correctManga(m)
+                }
+                m
+            }.filter { m ->
+                isMangaValid(m)
+            }
+        }
     }
 
     fun deleteManga(uri: String) {
