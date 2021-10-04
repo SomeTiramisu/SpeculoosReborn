97ce9499e0ba8d46acd10e1f8f8be607ae784d1d
SomeTiramisu <SomeTiramisu@users.noreply.github.com>
2021-10-04 15:50:11 +0200

    pdf support tweaks

--- app/src/main/java/org/custro/speculoosreborn/libtiramisuk/renderer/PdfRendererPage.kt
+++ app/src/main/java/org/custro/speculoosreborn/libtiramisuk/renderer/PdfRendererPage.kt
@@ -1,13 +1,19 @@
 package org.custro.speculoosreborn.libtiramisuk.renderer
 
 import android.graphics.Bitmap
+import android.graphics.Matrix
+import android.graphics.Rect
 import android.graphics.pdf.PdfRenderer
 import android.graphics.pdf.PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY
+import kotlin.math.min
 
 class PdfRendererPage(private val page: PdfRenderer.Page): RendererPage {
     override fun render(bitmap: Bitmap): RenderInfo {
-        bitmap.reconfigure(bitmap.width, bitmap.height, Bitmap.Config.ARGB_8888)
-        page.render(bitmap, null, null , RENDER_MODE_FOR_DISPLAY)
+        val fx = bitmap.width.toDouble()/page.width.toDouble()
+        val fy = bitmap.height.toDouble()/page.height.toDouble()
+        val f = min(fx, fy)
+        bitmap.reconfigure((page.width*f).toInt(), (page.height*f).toInt(), Bitmap.Config.ARGB_8888)
+        page.render(bitmap, null, Matrix().apply { setScale(15F, 15F);postTranslate(-5000F, -5000F) } , RENDER_MODE_FOR_DISPLAY)
         return RenderInfo(false)
     }
 
