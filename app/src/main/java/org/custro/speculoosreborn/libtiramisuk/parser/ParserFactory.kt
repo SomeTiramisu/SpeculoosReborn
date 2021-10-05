e8b6c732fa5134a0be3d2cacd8346bd4cce39d0f
SomeTiramisu <SomeTiramisu@users.noreply.github.com>
2021-10-05 16:02:57 +0200

    new pdf Parser

--- app/src/main/java/org/custro/speculoosreborn/libtiramisuk/parser/ParserFactory.kt
+++ app/src/main/java/org/custro/speculoosreborn/libtiramisuk/parser/ParserFactory.kt
@@ -9,13 +9,15 @@ class ParserFactory() {
             if (ZipStreamParser.isSupported(uri)) return ZipStreamParser(uri)
             if (RarFileParser.isSupported(uri)) return RarFileParser(uri)
             if (RarStreamParser.isSupported(uri)) return RarStreamParser(uri)
+            if (PdfFileParser.isSupported(uri)) return PdfFileParser(uri)
             throw Exception("Unsupported file")
         }
         fun isSupported(uri: Uri): Boolean {
             return ZipFileParser.isSupported(uri) or
                     ZipStreamParser.isSupported(uri) or
                     RarFileParser.isSupported(uri) or
-                    RarStreamParser.isSupported(uri)
+                    RarStreamParser.isSupported(uri) or
+                    PdfFileParser.isSupported(uri)
         }
     }
 }
---
+++ app/src/main/java/org/custro/speculoosreborn/libtiramisuk/parser/PdfFileParser.kt
@@ -0,0 +1,48 @@
+package org.custro.speculoosreborn.libtiramisuk.parser
+
+import android.net.Uri
+import android.util.Log
+import androidx.core.net.toFile
+import com.tom_roush.pdfbox.cos.COSName
+import com.tom_roush.pdfbox.pdmodel.PDDocument
+import com.tom_roush.pdfbox.pdmodel.graphics.image.PDImageXObject
+import org.custro.speculoosreborn.libtiramisuk.utils.bitmapToByteArray
+
+class PdfFileParser(override val uri: Uri) : Parser {
+    private val pdfFile = PDDocument.load(uri.toFile())
+    private val headers: MutableList<COSName> = mutableListOf()
+    override val size: Int
+        get() = headers.size
+
+    init {
+        Log.d("PdfFileParser", "scheme: ${uri.scheme}")
+        for (page in pdfFile.pages) {
+            val res = page.resources
+            for (name in res.xObjectNames) {
+                if (res.isImageXObject(name)) {
+                    headers.add(name)
+                }
+            }
+        }
+    }
+
+
+    override fun at(index: Int): ByteArray {
+        val imgObj = pdfFile.pages[index].resources.getXObject(headers[index]) as PDImageXObject
+        return bitmapToByteArray(imgObj.image)
+    }
+
+    override fun atRange(vararg indexes: Int): List<ByteArray> {
+        TODO("Not yet implemented")
+    }
+
+    override fun close() {
+        pdfFile.close()
+    }
+
+    companion object {
+        fun isSupported(uri: Uri) = uri.scheme == "file"
+                && uri.lastPathSegment?.lowercase()?.matches(Regex(".*\\.(pdf)$")) ?: false
+    }
+}
+
