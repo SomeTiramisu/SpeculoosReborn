d8d36de662dded51e8ffba0605c4a033aa5e632f
SomeTiramisu <SomeTiramisu@users.noreply.github.com>
2021-10-07 20:30:22 +0200

    InitScreen UI tweaks

--- app/src/main/java/org/custro/speculoosreborn/MangaCard.kt
+++ app/src/main/java/org/custro/speculoosreborn/MangaCard.kt
@@ -1,36 +1,40 @@
 package org.custro.speculoosreborn
 
 import android.net.Uri
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.TextButton
-import androidx.compose.material.Card
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material.icons.filled.RemoveCircleOutline
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+@ExperimentalFoundationApi
 @Composable
 fun MangaCard(model: MangaCardModel = MangaCardModel(), onRead: (uri: String) -> Unit, onDelete: (uri: String) -> Unit) {
+    var isMenuExpanded: Boolean by remember { mutableStateOf(false) }
     Card(
         modifier = Modifier
             .fillMaxWidth()
+            .combinedClickable(onClick = { onRead(model.localUri) },
+                onLongClick = {isMenuExpanded = true})
             .height(100.dp)
     ) {
         val cover: ImageBitmap by model.cover.observeAsState(ImageBitmap(1, 1))
         Row {
             Image(bitmap = cover, contentDescription = "front page")
             Column(verticalArrangement = Arrangement.Center,
-            modifier = Modifier.padding(8.dp)) {
+            modifier = Modifier.padding(8.dp).fillMaxSize()) {
                 Uri.parse(model.uri).lastPathSegment?.let { it1 ->
                     Text(
                         text = it1.split(':').last().split('/').last(),
@@ -39,7 +43,7 @@ fun MangaCard(model: MangaCardModel = MangaCardModel(), onRead: (uri: String) ->
                         maxLines = 1
                     )
                 }
-                Row {
+                /*Row {
                     TextButton(onClick = { onDelete(model.uri) },
                     modifier = Modifier.fillMaxHeight()
                     ) {
@@ -52,7 +56,12 @@ fun MangaCard(model: MangaCardModel = MangaCardModel(), onRead: (uri: String) ->
                         Text(text = "Read")
                         Icon(Icons.Filled.PlayArrow, "read manga" )
                     }
-                }
+                }*/
+            }
+        }
+        DropdownMenu(expanded = isMenuExpanded, onDismissRequest = { isMenuExpanded = false }) {
+            DropdownMenuItem(onClick = { onDelete(model.uri) }) {
+                Text(text = "Remove", color = Color.Red)
             }
         }
     }
