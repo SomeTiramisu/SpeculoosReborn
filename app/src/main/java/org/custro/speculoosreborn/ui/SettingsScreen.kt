7a194972cb5cfccbf86ee36bd9b84d36504224e7
SomeTiramisu <SomeTiramisu@users.noreply.github.com>
2022-01-27 14:50:39 +0100

    gradle update

--- app/src/main/java/org/custro/speculoosreborn/ui/SettingsScreen.kt
+++ app/src/main/java/org/custro/speculoosreborn/ui/SettingsScreen.kt
@@ -1,18 +1,21 @@
 package org.custro.speculoosreborn.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.viewmodel.compose.viewModel
 import org.custro.speculoosreborn.ui.model.SettingsModel
 
 @Composable
-fun SettingsScreen(model: SettingsModel = viewModel()) {/*
-    val bordersState = rememberPreferenceBooleanSettingState(key = "borders_pref_key", defaultValue = true)
-    val scaleState = rememberPreferenceBooleanSettingState(key = "scale_pref_key", defaultValue = true)
-    val maskState = rememberPreferenceBooleanSettingState(key = "mask_pref_key", defaultValue = true)
-    val cropState = rememberPreferenceBooleanSettingState(key = "crop_pref_key", defaultValue = true)
-    val backgroundState = rememberPreferenceStringSettingState(key = "background_pref_key", defaultValue = "file:///android_asset/background.png")
+fun SettingsScreen(model: SettingsModel = viewModel()) {
+    //val bordersState = rememberPreferenceBooleanSettingState(key = "borders_pref_key", defaultValue = true)
+    //val scaleState = rememberPreferenceBooleanSettingState(key = "scale_pref_key", defaultValue = true)
+    //val maskState = rememberPreferenceBooleanSettingState(key = "mask_pref_key", defaultValue = true)
+    //val cropState = rememberPreferenceBooleanSettingState(key = "crop_pref_key", defaultValue = true)
+    //val backgroundState = rememberPreferenceStringSettingState(key = "background_pref_key", defaultValue = "file:///android_asset/background.png")
     Surface {
         Column {
+            /*
             SettingsCheckbox(state = bordersState,
                 title = { Text("Borders") },
                 subtitle = { Text("Add black borders to fit screen") }) {
@@ -35,7 +38,7 @@ fun SettingsScreen(model: SettingsModel = viewModel()) {/*
             SettingsMenuLink(title = { Text("Reset library") }) {
 
             }
-
+*/
         }
-    }*/
+    }
 }
